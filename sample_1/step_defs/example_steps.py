c542b8ce75935ae1e626a5d503b0545eb2ef91f9
Sten Aksel Heien <sah@utel.tech>
2023-06-28 13:02:30 +0200

    pylint: disable=unused-arguments

--- sample_1/step_defs/example_steps.py
+++ sample_1/step_defs/example_steps.py
@@ -4,5 +4,6 @@ from pytest_bdd.parsers import parse
 
 @when(parse('{name} performs an {action}'))
 def step_function(name, action):
+    # pylint: disable=unused-argument
     # Add Your Code Here
     pass
